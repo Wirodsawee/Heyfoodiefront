ab07efe70c7b289291b4c2504f648a26de23dd1d
ITTIDATE CHANSATIT <ittidate.1998@mail.kmutt.ac.th>
2020-11-30 00:45:01 +0700

    Update Slide pic

--- src/component/Menu/SlideMenu.jsx
+++ src/component/Menu/SlideMenu.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import home from "../../img/home/home.jpg"
 import home2 from "../../img/home/home2.png"
+import home3 from "../../img/home/home3.png"
 import Carousel from "react-bootstrap/esm/Carousel"
 
 export default function SlideMenu() {
@@ -13,6 +14,9 @@ export default function SlideMenu() {
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={home2} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={home3} alt="Third slide" />
           </Carousel.Item>
         </Carousel>
       </div>
